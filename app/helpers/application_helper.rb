f29a8a3a01c20f2d22aa9638feb97a9131230e75
Shahzad Tariq <m.shahzad.tariq@hotmail.com>
2024-04-14 19:17:48 +0500

    renamed hello stimulus controller to user stimulus controller (hello stimulus is created by default in new project)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,18 +5,18 @@ module ApplicationHelper
     update_url = options[:url] || url_for(obj)
     case type
     when 'text'
-      tag = text_field_tag(field_name, obj.send(attr), data: { 'hello-target' => 'attr', 'action' => 'change->hello#autosave' })
+      tag = text_field_tag(field_name, obj.send(attr), data: { 'user-target' => 'attr', 'action' => 'change->user#autosave' })
     when 'date'
-      tag = date_field_tag(field_name, obj.send(attr), data: { 'hello-target' => 'attr', 'action' => 'change->hello#autosave' })
+      tag = date_field_tag(field_name, obj.send(attr), data: { 'user-target' => 'attr', 'action' => 'change->user#autosave' })
     when 'select'
       select_options = options[:options]
       selected = options[:selected] || obj.send(attr)
 
-      tag = select_tag('select', options_for_select(select_options, selected), data: { 'hello-target' => 'attr', 'action' => 'change->hello#autosave' })
+      tag = select_tag('select', options_for_select(select_options, selected), data: { 'user-target' => 'attr', 'action' => 'change->user#autosave' })
     end
 
     html = <<~HTML
-<div data-controller="hello" data-id=#{obj.id} data-url=#{update_url} data-field=#{attr}>
+<div data-controller="user" data-id=#{obj.id} data-url=#{update_url} data-field=#{attr}>
   #{tag}
 </div>
     HTML
